446112c81a5fc37b48f900a43f2ad09f3c76b538
Suraj Katyayan <katyayansuraj@gmail.com>
2023-07-27 05:44:25 +0530

    version 2 image errors

--- components/Caps.js
+++ components/Caps.js
@@ -1,17 +1,17 @@
-import Image from "next/image";
-import walletbanner from "../resources/header/caps.png";
-import styles from "./Caps.module.css";
+// import Image from "next/image";
+// import walletbanner from "../resources/header/caps.png";
+// import styles from "./Caps.module.css";
 
-export default function Caps() {
-  return (
-    <>
-      <Image
-        src={walletbanner}
-        width={1536}
-        height={600}
-        alt="banner for caps"
-        className={styles.capscontainer}
-      />
-    </>
-  );
-}
+// export default function Caps() {
+//   return (
+//     <>
+//       <Image
+//         src={walletbanner}
+//         width={1536}
+//         height={600}
+//         alt="banner for caps"
+//         className={styles.capscontainer}
+//       />
+//     </>
+//   );
+// }
